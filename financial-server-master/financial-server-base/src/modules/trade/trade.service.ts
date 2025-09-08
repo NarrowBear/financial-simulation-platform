295a06ebb6ad30d19f06bfff339c77f15fa644b7
NarrowBear <283998034@qq.com>
2025-09-08 19:31:34 +0800

    up ~

--- financial-server-master/financial-server-base/src/modules/trade/trade.service.ts
+++ financial-server-master/financial-server-base/src/modules/trade/trade.service.ts
@@ -9,6 +9,7 @@ import { OrderService } from "../order/order.service";
 import { DoubleHeapUtils } from "src/common/utils/double-heap.utils";
 import { RedisService } from "src/common/service/redis.service";
 import { v4 as uuidv4 } from 'uuid';
+import { min } from "rxjs";
 
 @Injectable()
 export class TradeService extends BaseService<TradeRecord> {
@@ -154,23 +155,68 @@ export class TradeService extends BaseService<TradeRecord> {
         // 2. buy order is limit order, sell order is market order
         // 3. buy order is market order, sell order is limit order
         if(buyOrder.orderType === OrderType.MARKET && sellOrder.orderType === OrderType.MARKET) {
+            situation = 1;
             maxBuyPrice = buyOrder.price * (1 + buyOrder.maxSlippage / 100);
-            minBuyPrice = buyOrder.price * (1 - buyOrder.minSlippage / 100);
-            maxSellPrice = sellOrder.price * (1 + sellOrder.maxSlippage / 100);
-            minSellPrice = sellOrder.price * (1 - sellOrder.minSlippage / 100);
+            minBuyPrice = buyOrder.price;
+            maxSellPrice = sellOrder.maxSlippage;
+            minSellPrice = sellOrder.price * (1 - sellOrder.maxSlippage / 100);
         } else if(buyOrder.orderType === OrderType.LIMIT && sellOrder.orderType === OrderType.MARKET) {
-            maxBuyPrice = buyOrder.price;
-            maxSellPrice = sellOrder.price * (1 + sellOrder.maxSlippage / 100);
-            minSellPrice = sellOrder.price * (1 - sellOrder.minSlippage / 100);
+            situation = 2;
+            minBuyPrice = buyOrder.price;
+            maxSellPrice = sellOrder.price * (1 - sellOrder.maxSlippage / 100);
+            minSellPrice = sellOrder.price;
         } else if(buyOrder.orderType === OrderType.MARKET && sellOrder.orderType === OrderType.LIMIT) {
+            situation = 3;
             maxBuyPrice = buyOrder.price * (1 + buyOrder.maxSlippage / 100);
-            minBuyPrice = buyOrder.price * (1 - buyOrder.minSlippage / 100);
-            maxSellPrice = sellOrder.price;
+            minSellPrice = sellOrder.price;
         }
-        
+        const result = await this.slippage(situation, maxBuyPrice, minBuyPrice, maxSellPrice, minSellPrice, sellOrder, buyOrder);
+        return result;
     }
 
     /**
+     * Slippage check
+     * @param situation 
+     * @param maxBuyPrice 
+     * @param minBuyPrice 
+     * @param maxSellPrice 
+     * @param minSellPrice 
+     * @param sellOrder 
+     * @param buyOrder 
+     */
+    async slippage(situation: number, maxBuyPrice: number, minBuyPrice: number, maxSellPrice: number, minSellPrice: number, sellOrder: Order, buyOrder: Order): Promise<any> {
+        const result = {
+            isMatch: false,
+            buyPrice: 0,
+            sellPrice: 0
+        };
+        if(situation === 1) {
+            // need to check the price is in the range of maxBuyPrice and minBuyPrice
+            if(minBuyPrice <= maxSellPrice && maxBuyPrice >= minSellPrice) {
+                result.isMatch = true;
+                result.buyPrice = Math.max(minBuyPrice, minSellPrice);
+                result.sellPrice = Math.min(maxBuyPrice, maxSellPrice);
+                const middlePrice = (result.buyPrice + result.sellPrice) / 2;
+                result.buyPrice = middlePrice;
+                result.sellPrice = middlePrice;
+            }
+        } else if (situation === 2) {
+            if(minBuyPrice >= minSellPrice) {
+                result.isMatch = true;
+                result.buyPrice = minBuyPrice;
+                result.sellPrice = minBuyPrice;
+            }
+        } else if (situation === 3) {
+            if(maxBuyPrice >= minSellPrice) {
+                result.isMatch = true;
+                result.buyPrice = minSellPrice;
+                result.sellPrice = minSellPrice;
+            }
+        }
+        return result;
+    }
+
+    /**-+
      * Create trade record
      * @param buyOrder 
      * @param sellOrder 
